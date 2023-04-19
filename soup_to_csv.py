5943b03d6ba344e4a0d16937a3475c996ae3cbcf
Miguel Salazar <miguelsalazar88@gmail.com>
2023-04-19 11:55:51 -0500

    Soup el tiempo funcional

--- soup_to_csv.py
+++ soup_to_csv.py
@@ -6,24 +6,30 @@ date = now.strftime('%Y-%m-%d')
 link_head_el_tiempo = 'https://www.eltiempo.com'
 link_head_el_espectador = 'https://www.elespectador.com'
 
-def extract_tiempo(doc_el_tiempo):
+def extract_tiempo(doc_el_tiempo, csv_el_tiempo):
 
     with open(doc_el_tiempo) as fp:
         soup = BeautifulSoup(fp, 'html.parser')
     headlines = soup.find_all(class_='title page-link')
-    print(len(headlines))
-    for headline in headlines:
-        title = headline.text
-        headless_link = headline['href']
-        category = headless_link.split('/')[1]
-        full_link = f''
+    with open(csv_el_tiempo, 'w') as csv:
+        csv.write('categoria, titular, enlace\n')
+        for headline in headlines:
+            title = headline.text
+            headless_link = headline['href']
+            category = headless_link.split('/')[1]
+            full_link = f'{link_head_el_tiempo}{headless_link}'
+            csv.write(f'{category},{title},{full_link},\n')
 
-
+def extract_espectador(doc_el_espectador, link_el_espectador):
+    pass
 
 doc_el_espectador = f'/Users/miguelsalazar/Desktop/big_data/parcial2/headlines-raw/el_espectador/contenido-{date}.html'
 doc_el_tiempo = f'/Users/miguelsalazar/Desktop/big_data/parcial2/headlines-raw/el_tiempo/contenido-{date}.html'
 
-extract_tiempo(doc_el_tiempo)
+csv_el_espectador = f'/Users/miguelsalazar/Desktop/big_data/parcial2/headlines-final/el_espectador/contenido-{date}.csv'
+csv_el_tiempo = f'/Users/miguelsalazar/Desktop/big_data/parcial2/headlines-final/el_tiempo/contenido-{date}.csv'
+
+extract_tiempo(doc_el_tiempo, csv_el_tiempo)
 
 
 
